bae21694db340c9dde69731a2dacf9ae6f30f1d2
Alicia <tucorreo@mail.com>
2019-01-07 21:28:56 -0300

    resolution of exercise 2

---
+++ ejercicio2.rb
@@ -0,0 +1,17 @@
+
+class MiClase
+    attr_accessor :name
+   
+      def initialize(name)
+        @name = name
+      end
+  
+    def saludar
+     puts "Hola! Soy la clase #{@name}"
+    end
+   
+   end
+  
+   c = MiClase.new('Clase Uno')
+   c.name = 'MiClase'
+   puts c.saludar
